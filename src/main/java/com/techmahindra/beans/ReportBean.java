66447dd4ab40800704fa1b71de53b86e7b5e3df3
Rajendra <rajendrachowdary40@gmail.com>
2019-10-25 10:52:54 +0530

    controllerupdated

--- src/main/java/com/techmahindra/beans/ReportBean.java
+++ src/main/java/com/techmahindra/beans/ReportBean.java
@@ -3,18 +3,18 @@ package com.techmahindra.beans;
 public class ReportBean 
 {
 private int customer_id;
-private int product;
+private int product_id;
 public int getcustomer_id() {
 	return customer_id;
 }
 public void setcustomer_id(int customer_id) {
 	this.customer_id = customer_id;
 }
-public int getproduct() {
-	return product;
+public int getproduct_id() {
+	return product_id;
 }
-public void setproduct(int product) {
-	this.product = product;
+public void setproduct_id(int product_id) {
+	this.product_id = product_id;
 }
 
 }
